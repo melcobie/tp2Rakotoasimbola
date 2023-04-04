3a4b6478126901a79da1b10f9fb35cbd8e51f7ad
Mel <asimbolalalaina@gmail.com>
2023-04-04 12:07:53 +0300

    Création initiale template

--- src/main/java/mg/itu/rakotoasimbola/tp2rakotoasimbola/jsf/OtherBean.java
+++ src/main/java/mg/itu/rakotoasimbola/tp2rakotoasimbola/jsf/OtherBean.java
@@ -39,5 +39,9 @@ public class OtherBean {
     public String afficher() {
         return "affichage_4?nb=" + nombre + "&amp;faces-redirect=true";
     }
+    
+    public String afficherTemplate() {
+        return "affichage__4?nb=" + nombre + "&amp;faces-redirect=true";
+    }
 
 }
